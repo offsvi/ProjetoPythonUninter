fca97bf5cb541120029065f03d0f0d8de29b7e56
Vitor Oliveira <vitorsk807@gmail.com>
2023-02-24 22:23:12 -0300

    Criando código e atualizando listas

--- CadastrarProduto.py
+++ CadastrarProduto.py
@@ -1,36 +1,36 @@
-listaProdutos = [] #Essa lista é uma variavel global
-#----------COMEÇO cadastrarProduto ----------
-def cadastrarProduto(codigo):
-    print ('Bem vindo ao controle de estoque da mercearia Vitor Oliveira')
-    print('O codigo do produto a ser cadastrado é: {}'.format(codigo))
+listaProdutos = []
+#-----------Começo CadastrarProdutos--------------
+def CadastrarProdutos(codigo):
+    print('Bem vindo ao controle de estoque')
+    print('O codigo do produti a ser cadastrado é: {}'.format(codigo))
     nome = input('Por favor digite o nome do produto: ')
-    fabricante = input('Por favor digite o fabricante do produto: ')
+    fabricante = input('Por favo digite o nome do fabricante do produto: ')
     valor = input('Por favor informe o valor do produto: ')
-    dicionarioProduto = {'codigo'     : codigo,  #Dicionario lista o código, nome,fabricante, valor
-                         'nome'       : nome,
-                         'fabricante' : fabricante,
-                         'valor'      : valor}
-    listaProdutos.append(dicionarioProduto.copy()) #Adiciona uma cópia do dicionário
-#----------FIM cadastrarProduto----------
+    dicionarioProduto = {'codigo'     :codigo,   #Dicionario lista o codigo, nome, fabricante e valor
+                        'nome'        : nome,
+                        'fabricante'  : fabricante,
+                        'valor'       : valor}
+    listaProdutos.append(dicionarioProduto.copy()) #Adciona uma cópia do dicionário
+#------------ Fim cadastrarProduto--------------------
 
-#----------COMEÇO consultarProdutos----------
+#------------ Começo consultarProdutos ----------------
 def consultarProdutos():
     while True:
-        try: #Será protegido por qualquer erro de digitação
-            print ('Bem-vindo a consulta de produtos')
-            opConsultar = int(input('Informe a opção desejada:\n1- Consultar todos os produtos\n2- Consultar produto por código:\n3- Consultar produto por fabricante:\n4- Retornar\n>>'))
+        try:
+            print('Bem vindo a consulta de produtos!')
+            opConsultar = int(input('Informe a opção desejada: \n1- Consultar todos os produtos\n2- Consultar produto por código:\n3- Consultar produto por fabricante:\n4- Retornar\n>> '))
             if opConsultar == 1:
-                print('Você selecionou a opção - consultar todos os produtos-')
-                for produto in listaProdutos: #Selecionar cada dicionario da minha lista (Cada produto da lista de produtos)
-                    for key, value in produto.items(): #Selecionar cada conjunto chave/valor do dicionario
-                        print('{} : {}'.format(key,value))
+                print('Você selecionou a opção - Consultar todos os produtos-')
+                for produto in listaProdutos:
+                    for key, Value in produto.items(): 
+                        print('{}  :  {}'.format(key, Value))
             elif opConsultar == 2:
-                print('Você selecionou a opção -consultar produto por código-')
-                entrada = int(input('Informe o código desejado:'))
+                print('Você selecionou a opção - Consultar produto por código-')
+                entrada = int(input('Informe o código desejado: '))
                 for produto in listaProdutos:
                     if (produto['codigo'] == entrada):
-                        for key, value in produto.items(): #Selecionar cada conjunto chave/valor do dicionario
-                            print('{} : {}'.format(key, value))
+                        for key, value in produto.items():
+                            print('{}  :  {}'.format(key, value))
             elif opConsultar == 3:
                 print('Você selecionou a opção -Consultar produto por fabricante-')
                 entrada = input('Informe o fabricante desejado:')
@@ -44,27 +44,26 @@ def consultarProdutos():
                 print('Número invalido. Digite a opção correta!')
                 continue
         except ValueError:
-            print('Pare de digitar valores que são inteiros!')
-#----------FIM consultarProduto----------
+            print('Pare de digitar que não são inteiros!')
+#---------------Fim Consulta Produto--------------
 
-#----------COMEÇO removerProduto----------
-def removerProduto():
-    print ('Bem-vindo a remover produtos')
-    entrada = int(input('Informe o código desejado:'))
-    for produto in listaProdutos:
-        if (produto['codigo'] == entrada):
-            listaProdutos.remove(produto)
-#----------FIM removerProduto----------
+#---------------Começo Remover Produto--------------
+removerProduto():
+print('Bem vindo a remover produtos')
+entrada = int(input('Inform o código desejado: '))
+for produto in listaProdutos:
+    if (produto['codigo'] == entrada):
+        listaProdutos.remove(produto)
+#-------------Fim Lista de produtos----------------
 
-#----------COMEÇO DA MAIN----------
-print('Bem vindo ao controle de estoque da mercearia Vitor Oliveira')
-registroProduto = 0 #Importante começar com 0 porque irá servir como acumulador
+#------------- Começo da Main--------------
+print('Bem vindo ao controle de estoque')
+registroProduto = 0
 while True:
-    try: #Será protegido por qualquer erro de digitação
-        opcao = int(input('Informe a opção desejada: \n1- Cadastrar Produtos\n2- Consultar produto por código\n3- Remover produto\n4- Sair\n>>'))
+    try:
+        opcao = int(input('Informe a opção desejada: \n1- Cadastrar produtos\n2- Consultar produtos por código\n3- Remover produtos\n4- Sair\n>>'))
         if opcao == 1:
             registroProduto = registroProduto + 1
-            cadastrarProduto(registroProduto)
         elif opcao == 2:
             registroProduto = registroProduto + 2
             consultarProdutos()
@@ -76,8 +75,8 @@ while True:
             print('Programa encerrado')
             break
         else:
-            print('Número invalido. Digite a opção correta!')
+            print('Número invalido. Digite a opção correta!!!')
             continue
     except ValueError:
-        print('Pare de digitar valores que são inteiros!')
-#----------FIM DA MAIN----------
+        print('Pare de digitar valores que são incorretos!!!')
+#------------- Fim da Main ----------------
